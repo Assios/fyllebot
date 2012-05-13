c8a9e0ed71e193bbefaf0fcd61b6657a1fac6391
Stian Jensen <stianjensen2000@gmail.com>
2012-05-13 20:56:45 +0200

    More cleanup (mat.py)

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ import urllib
 import shlex
 from time import sleep
 from imdb import *
+from mat import *
 
 network = 'irc.quakenet.org'
 port = 6667
@@ -23,41 +24,12 @@ irc.send ( 'USER fyllebot fyllebot fyllebot :FylleBOOOT\r\n' )
 irc.send ( 'JOIN #fyllechat\r\n' )
 irc.send ( 'PRIVMSG #fyllechat :HEI ASS.\r\n' )
 
-middag = {'laks': ['eburger', 'sitronmarinert -', 'egrandis', ' i fiskesuppe', 'vegetar-'], 'grandis':[''], 'burger':['tallerken', ' med cornflakes', 'kylling-'], 'kylling':[' og chips', ' og ris', 'gryte', 'vinger'], 'pyttipanne':['fløtegratinert -'], 'kebab':[' i pita', 'rulle-', 'vegetar-'], 'lasagne':['tysk -', 'fransk -'], 'spagetti':[' bolognaise', ' carbonara'], 'tomatsuppe':[' og biff', 'gratinert -'], 'taco':['mexikansk -', 'spansk -', 'svensk -'], 'pølse':['blod-','grise-','kjøtt-', ' og potetmos'], 'pannekaker':['bacon-',' med sirup'], 'shake':['protein-', 'kebab-']}
-drikke = {'shots': ['jäger-', 'vodka-', 'minttu-', 'tequila-'], 'vin':['rød-', 'hvit-'], 'martini':['shaken -', 'stirred -', ', shaken, not stirred'], 'whiskey':['scottish -', 'irish -'], 'akevitt':['', 'jule-']}
-
 dato = str(datetime.datetime.now())
 aar = int(dato[0:4])
 maaned = int(dato[5:7])
 dag = int(dato[8:10])
 dato = datetime.date(aar, maaned, dag)
 polet = [17, 17, 17, 18, 18, 15, 10000]
-
-def maat():
-   hovedrett = middag.keys()
-   retten = hovedrett[random.randint(0,len(hovedrett)-1)]
-   tilbehor = middag[retten]
-   tilbehoret = tilbehor[random.randint(0,len(tilbehor)-1)]
-   mat = retten + tilbehoret
-   if mat[-1]=='-':
-      mat = tilbehoret.strip('-')+retten
-   send('Du skal spise ' + mat)
-
-def drikkee():
-   hoveddrikke = drikke.keys()
-   drikk = hoveddrikke[random.randint(0,len(hoveddrikke)-1)]
-   drikketillegg = drikke[drikk]
-   drikketillegget = drikketillegg[random.randint(0,len(drikketillegg)-1)]
-   drikken = drikk+drikketillegget
-   if drikken[-1]=='-':
-      drikken = drikketillegget.strip('-')+drikk
-   if drikken[-1]+drikken[-2]+drikken[-3] == 'sto':
-      send('Drikk '+ str(random.randint(2,10))+ ' ' + drikken)
-      if drikken[0:3]=='teq':
-         send('Husk salt og sitron a!')
-   else:
-      send('Drikk ' + drikken)
-
 
 def stengetidpolet():
     aapent = 1
---
+++ mat.py
@@ -0,0 +1,27 @@
+middag = {'laks': ['eburger', 'sitronmarinert -', 'egrandis', ' i fiskesuppe', 'vegetar-'], 'grandis':[''], 'burger':['tallerken', ' med cornflakes', 'kylling-'], 'kylling':[' og chips', ' og ris', 'gryte', 'vinger'], 'pyttipanne':['fløtegratinert -'], 'kebab':[' i pita', 'rulle-', 'vegetar-'], 'lasagne':['tysk -', 'fransk -'], 'spagetti':[' bolognaise', ' carbonara'], 'tomatsuppe':[' og biff', 'gratinert -'], 'taco':['mexikansk -', 'spansk -', 'svensk -'], 'pølse':['blod-','grise-','kjøtt-', ' og potetmos'], 'pannekaker':['bacon-',' med sirup'], 'shake':['protein-', 'kebab-']}
+drikke = {'shots': ['jäger-', 'vodka-', 'minttu-', 'tequila-'], 'vin':['rød-', 'hvit-'], 'martini':['shaken -', 'stirred -', ', shaken, not stirred'], 'whiskey':['scottish -', 'irish -'], 'akevitt':['', 'jule-']}
+
+def maat():
+   hovedrett = middag.keys()
+   retten = hovedrett[random.randint(0,len(hovedrett)-1)]
+   tilbehor = middag[retten]
+   tilbehoret = tilbehor[random.randint(0,len(tilbehor)-1)]
+   mat = retten + tilbehoret
+   if mat[-1]=='-':
+      mat = tilbehoret.strip('-')+retten
+   send('Du skal spise ' + mat)
+
+def drikkee():
+   hoveddrikke = drikke.keys()
+   drikk = hoveddrikke[random.randint(0,len(hoveddrikke)-1)]
+   drikketillegg = drikke[drikk]
+   drikketillegget = drikketillegg[random.randint(0,len(drikketillegg)-1)]
+   drikken = drikk+drikketillegget
+   if drikken[-1]=='-':
+      drikken = drikketillegget.strip('-')+drikk
+   if drikken[-1]+drikken[-2]+drikken[-3] == 'sto':
+      send('Drikk '+ str(random.randint(2,10))+ ' ' + drikken)
+      if drikken[0:3]=='teq':
+         send('Husk salt og sitron a!')
+   else:
+      send('Drikk ' + drikken)
