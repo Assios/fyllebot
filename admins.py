4bf08c22eee94dde3edcc05f00b7b8750c89cb85
Asbjorn Steinskog <asbjornsteinskog@gmail.com>
2012-05-21 16:50:59 +0200

    stuff

--- admins.py
+++ admins.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
+def kickUsers(username, melding):
+   return('KICK ' + " #fyllechat " + username + " :" + melding + '\r\n')
 
 def opUser(username):
    return('O ' + " #fyllechat " + username + '\r\n')
