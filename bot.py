ef3cfd8af7a827d6202c1750b50a159709685dca
Asbjorn Steinskog <asbjornsteinskog@gmail.com>
2012-05-24 23:09:52 +0200

    This should work

--- bot.py
+++ bot.py
@@ -418,7 +418,7 @@ while True:
       continue
 
    if ('http://' in message) and not (user == 'fyllebot') and (finishedLoading == 1):
-      send(urlTitle(returnURLs(message)[0]))
+      send(returnURLs[0])
       
 
    #if (smallTalk == 1) and (user == brukerTalk):
