ab663527e23c1cfe85e80235a8c885eab28f5bdd
Asbjørn Steinskog <asbjornsteinskog@gmail.com>
2012-06-30 02:43:08 +0200

    Funker nå med mellomrom i regnestykkene

--- bot.py
+++ bot.py
@@ -513,7 +513,7 @@ while True:
 
    try:
       if (shlex.split(message)[0]=='!kalkuler'):
-            send(calculate(shlex.split(message)[1]))
+            send(calculate(message.split(' ', 1).strip()))
    except:
       pass
 
